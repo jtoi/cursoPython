c236fb02b7809550423e3941493663a44488b984
julio <jtoirac@gmail.com>
2023-03-22 06:43:24 +0100

    casa20230322

--- ObjectOriented(OO)/ejercicio18_composicion.py
+++ ObjectOriented(OO)/ejercicio18_composicion.py
@@ -25,7 +25,11 @@ class Empresa:
         self.director = director
         self.__departamentos = []
     def mostrar(self):
-        print(f"Empresa: {self.nombre} - Director: {self.director}")
+        print(f"Empresa: {self.nombre}")
+        print(f"Director: ", end='')
+        self.director.mostrar()
+        for departamento in self.__departamentos:
+            departamento.mostrar()
 
     def agregarDpto(self, departamento):
         self.__departamentos.append(departamento)
@@ -35,15 +39,54 @@ class Multinacional:
     def __init__(self, nombre, director):
         self.nombre = nombre
         self.director = director
+        self.__empresas = []
     def mostrar(self):
         print(f"Multinacional: {self.nombre}")
+        print(f"Director: ", end='')
+        self.director.mostrar()
+        for empresa in self.__empresas:
+            empresa.mostrar()
+
+    def agregarEmpre(self, empresa):
+        self.__empresas.append(empresa)
+
 
 
 
 
 victor = Empleado("Victor")
 laura = Empleado("laura")
+antonio = Empleado("Antonio")
+maria = Empleado("María")
+maricarmen = Empleado("Maricarmen")
+david= Empleado("David")
+javier= Empleado("Javier")
+josefa= Empleado("Josefa")
 dpto_sistemas = Departamento("Sistemas", Empleado('Manuel'))
 dpto_sistemas.agregarEmpleado(victor)
 dpto_sistemas.agregarEmpleado(laura)
-dpto_sistemas.mostrar()
+# dpto_sistemas.mostrar()
+dpto_desarrollo = Departamento("Desarrollo", Empleado('José'))
+dpto_desarrollo.agregarEmpleado(antonio)
+dpto_desarrollo.agregarEmpleado(maria)
+# dpto_sistemas.mostrar()
+empresa1 = Empresa("Empresa1", Empleado('Julio'))
+empresa1.agregarDpto(dpto_sistemas)
+empresa1.agregarDpto(dpto_desarrollo)
+
+dpto_sistemas2 = Departamento("Sistemas", Empleado('Maria Pilar'))
+dpto_sistemas2.agregarEmpleado(maricarmen)
+dpto_sistemas2.agregarEmpleado(david)
+# dpto_sistemas.mostrar()
+dpto_desarrollo2 = Departamento("Desarrollo", Empleado('Juan'))
+dpto_desarrollo2.agregarEmpleado(javier)
+dpto_desarrollo2.agregarEmpleado(josefa)
+# dpto_sistemas.mostrar()
+empresa2 = Empresa("Empresa2", Empleado('Julio'))
+empresa2.agregarDpto(dpto_sistemas2)
+empresa2.agregarDpto(dpto_desarrollo2)
+
+holding = Multinacional("Holding", Empleado('Francisco'))
+holding.agregarEmpre(empresa1)
+holding.agregarEmpre(empresa2)
+holding.mostrar()
