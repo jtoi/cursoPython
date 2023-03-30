2a61997c2d8b62569ea31c70c35b1eb14b304f8c
julio <jtoirac@gmail.com>
2023-03-30 19:43:22 +0200

    2023-03-30

--- Ejemplos_2023_03/ejemplo12.py/programa_consumidor.py
+++ Ejemplos_2023_03/ejemplo12.py/programa_consumidor.py
@@ -1,4 +1,8 @@
 import os
+from datetime import datetime
 
 retorno = os.system("python.exe programa_proveedor.py botella")
-print(retorno)
+# print(retorno)
+
+fecha=datetime.strptime("2035-01-01", '%Y-%m-%d')
+unix=datetime.timestamp(fecha)
